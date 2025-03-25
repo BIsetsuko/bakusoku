147a9833673b4fcc3e4021df7f76625f3f438c3a
BIsetsuko <seina.nagato@blueish.co.jp>
2025-03-25 22:50:03 +0900

    feat: getNarutoListを作成し、処理を関数へ切り出し

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"bakusoku/controller"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
-	router.GET("ping", func(c *gin.Context){
+
+	router.GET("/ping", func(c *gin.Context){
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+	router.GET("/getNarutoList", controller.Getting)
+	// router.POST("/createNarutoList", posting)
+	// router.DELETE("/deleteNarutoList", deleting)
+	// router.PUT("/updateNarutoList", putting)
+
 	router.Run(":4000")}
