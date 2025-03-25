fcd2737207b612e9051c88776d44ef4f0866100a
BIsetsuko <seina.nagato@blueish.co.jp>
2025-03-25 23:10:38 +0900

    feat: getNarutoListエンドポイントを追加

--- main.go
+++ main.go
@@ -4,9 +4,16 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	router := gin.Default()
-	router.GET("ping", func(c *gin.Context){
+	router.GET("/ping", func(c *gin.Context){
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	router.Run(":4000")}
+	router.GET("/getNarutoList", func(c *gin.Context){
+		c.JSON(200, gin.H{
+			"message": "ok",
+			"data": "500",
+		})
+	})
+	router.Run(":4000")
+}
